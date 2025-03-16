8c0cf11bb2af2f474ce903592a6674545776fa1a
Parth Sharma <parth1999sharma@gmail.com>
2025-03-16 10:48:57 +0000

    setting kar di

--- app/config/config.py
+++ app/config/config.py
@@ -16,6 +16,10 @@ class Settings(BaseSettings):
     MONGO_INITDB_ROOT_PASSWORD: Optional[str] = None
     MONGO_URI: Optional[str] = None
     MONGO_DB: Optional[str] = None
+    SECRET_KEY: Optional[str] = None  # Replace with a secure secret key in production
+    ALGORITHM: Optional[str] = None
+    ACCESS_TOKEN_EXPIRE_MINUTES: Optional[str] = None
+    REFRESH_TOKEN_EXPIRE_DAYS: Optional[str] = None
 
     ALLOWED_ORIGINS: Optional[str] = None
 
